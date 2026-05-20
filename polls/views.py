715a314e749c06df07effc54de538eaa653ade98
jPadua371 <joaopadua43432@gmail.com>
2026-05-20 12:08:14 -0300

    tentando 3

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Página inicial das enquetes")
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("Resultados da enquete")
+
+
+def vote(request, question_id):
+    return HttpResponse("Votação da enquete")
